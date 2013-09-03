4605f5286905ed9a5cfd1e1edc3083cd11153b2b
ton <dev@appsenzo.com>
2013-09-03 05:17:04 +0200

    Initial

---
+++ GerDuino.ino
@@ -0,0 +1,18 @@
+/*
+GerDuino
+My attempt at an electronic garden.
+
+03 sept 2013 initial version
+
+Ton with additional code from various public examples
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
